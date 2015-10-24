47d32137910716fa30fe729dc40fa8819dbb5229
weezing <weezing@protonmail.com>
2015-10-24 11:43:03 +0200

    Add Events controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023142601) do
+ActiveRecord::Schema.define(version: 20151024093703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "place"
+    t.datetime "start_time"
+    t.datetime "user_deadline"
+    t.string   "name"
+    t.text     "description"
+    t.integer  "slots"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
