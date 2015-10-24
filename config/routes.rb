a50026ea676e9eb6d4bf80b87bc04ecec517e02e
Pawel Licznar <pawellicznar@gmail.com>
2015-10-24 16:14:10 +0200

    Hide button for not logged

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'visitors#index'
+  root to: 'events#index'
 
   resources :events
 
