202e4aa6d3127621aa1bc2bd579eb1a82d5f8069
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-24 14:33:00 +0200

    Add user and event from admin panel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :list, :of, :attributes, :on, :model, :name, :email, :password, :password_confirmation
 #
 # or
 #
@@ -12,6 +12,16 @@ ActiveAdmin.register User do
 #   permitted << :other if resource.something?
 #   permitted
 # end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 
 
 end
