a7517a076606fa37a709e94a405e1bb83eb33fe2
weezing <weezing@protonmail.com>
2015-10-24 15:17:02 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,37 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+puts 'Seeds: start'
+
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    password: '12345678',
+    email: Faker::Internet.email
+  )
+  user.add_role :student
+  user.save
+end
+
+teacher = User.new(
+	name: Faker::Name.name,
+  password: '12345678',
+  email: Faker::Internet.email
+)
+teacher.add_role :teacher
+teacher.save
+
+6.times do
+  event = Event.new(
+    place: Faker::Address.city,
+    start_time: Faker::Date.between(5.days.since, 40.days.since),
+    user_deadline: Faker::Date.between(2.days.since, 35.days.since),
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.sentences,
+    slots: Faker::Number.between(10, 30)
+  )
+  event.save
+end
+
+puts 'Seeds: done'
