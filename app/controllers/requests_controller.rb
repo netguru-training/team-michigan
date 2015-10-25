829cd08e11b251937714fd433cdf51d48724b6a8
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-25 10:48:13 +0100

    Add sign_up for workshops option

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,14 @@
 class RequestsController < ApplicationController
   def create
-
+    event = Event.find(params['event'])
+    request = Request.create(user: current_user, event: event)
+    if request.save &&  event.slots > 0
+      event.slots -= 1
+      event.save
+      flash[:success] = "Joined to workshops!"
+    else
+      flash[:danger] = "Couldn't join to workshops. No empty slots!"
+    end
+    redirect_to event_path(params['event'])
   end
 end
