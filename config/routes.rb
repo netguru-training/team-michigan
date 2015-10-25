c49ce1c7455ccee4c216666f2a3cd4919fdcba96
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-25 10:17:42 +0100

    Add route to create request method

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'requests/create'
+  post 'requests' => 'requests#create'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'visitors#index'
 
   resources :events
-
-  resources :requests, only: :create
 
   devise_for :users, controllers: {
         registrations: 'users/registrations'
