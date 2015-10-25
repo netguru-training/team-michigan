7e82cd8aa4d8edd67937cedb53eba356bbe070bb
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-25 10:06:07 +0100

    Add methods for fetching student and teacher from user

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ActiveRecord::Base
   has_many :requests
   has_many :users, through: :requests
   belongs_to :user
+
+  def students
+    self.users
+  end
+
+  def teacher
+    self.user
+  end
 end
