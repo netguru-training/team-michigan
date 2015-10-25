872ca8febe3f505011a887815fe7d900c683f76d
Pawel Licznar <pawellicznar@gmail.com>
2015-10-25 11:47:48 +0100

    Add scopes to requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,8 @@
 class Request < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  scope :pending, -> {where(status: 'pending')}
+  scope :accepted, -> {where(status: 'accepted')}
+  scope :rejected, -> {where(status: 'rejected')}
 end
