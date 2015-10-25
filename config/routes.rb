14e865154eaac6c745aced7bc5d728c1ef86ed9f
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-25 12:21:48 +0100

    Add routes for evaluation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'visitors#index'
+
   post 'requests' => 'requests#create'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'visitors#index'
+
 
   resources :events
+  resources :evaluations, only: [:create, :new]
 
   devise_for :users, controllers: {
         registrations: 'users/registrations'
