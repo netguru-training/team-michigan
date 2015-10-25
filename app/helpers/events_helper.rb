8196c1f28165218a9ff7a439ab8772e21c4a26f2
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-25 14:55:30 +0100

    Fix displaying evaluations for unlogged user

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
-  def evaluation_for_student_exist?(event)
-    !Evaluation.where(event_id: event.id, user_id: current_user.id).empty? && (current_user.has_role? :student)
+  def evaluation_for_logged_student_exist?(event)
+    user_signed_in? && !Evaluation.where(event_id: event.id, user_id: current_user.id).empty? && (current_user.has_role? :student)
   end
 
   def evaluation(event)
