864302cf3f5a138c3b2edba32371abbbbecbae63
Mateusz Piechowiak <mateusz.s.piechowiak@gmail.com>
2015-10-25 11:21:01 +0100

    Fix sign up for workshops method
    Particular student was able to sign up for the same workshops many
    times which was incorrect

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,14 +1,19 @@
 class RequestsController < ApplicationController
+
   def create
     event = Event.find(params['event'])
-    request = Request.create(user: current_user, event: event)
-    if request.save &&  event.slots > 0
+
+    if event.students.include?(current_user)
+      flash[:danger] = 'Already joined to workshops!'
+    elsif event.slots == 0
+      flash[:danger] = 'No empty slots!'
+    else
       event.slots -= 1
       event.save
-      flash[:success] = "Joined to workshops!"
-    else
-      flash[:danger] = "Couldn't join to workshops. No empty slots!"
+      event.users << current_user
+      flash[:notice] = 'Joined to workshops!'
     end
+
     redirect_to event_path(params['event'])
   end
 end
