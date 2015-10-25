4aec31a0e84d1958f1e82fb34dfc3e6fc1f9efc8
Pawel Licznar <pawellicznar@gmail.com>
2015-10-25 15:29:03 +0100

    Add validations

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,4 +1,8 @@
 class Evaluation < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+
+  validates :reading, inclusion: 1..10
+  validates :writing, inclusion: 1..10
+  validates :listening, inclusion: 1..10
 end
